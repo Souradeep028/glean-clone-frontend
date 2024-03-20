10e0d630a5742ee2d5a2c5d165af45a5b5748a65
Souradeep <souradeepkebasu@gmail.com>
2024-03-20 21:56:15 +0400

    FEAT: updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import SearchBar from '@/components/searchBar';
 export default function Home() {
 	return (
 		<div className='bg-[#F0D6A9] min-h-screen flex flex-col items-center justify-start py-20 px-6'>
-			<h2 className='text-3xl font-bold mb-6 text-[#621e68]'>Glen Clone</h2>
+			<h2 className='text-3xl font-bold mb-6 text-[#621e68]'>Glean Clone</h2>
 			<SearchBar />
 		</div>
 	);
